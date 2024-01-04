10b38a99bec0afd52ea06a4fb78b656e05e94b3f
yasiu <hi@y4s.io>
2024-01-04 13:18:12 +0000

    tweak restore from emmc

--- src/home/exp266/helper/restore_from_emmc_tar.sh
+++ src/home/exp266/helper/restore_from_emmc_tar.sh
@@ -8,6 +8,6 @@ mkdir -p $output_path
 
 dd if=/dev/mmcblk0 bs=512 skip=13680640 count=376832 of=$output_path/sdr_recording_restored_${restored_date}.tar.gz
 #dd if=/dev/mmcblk0 bs=512 skip=13680640 count=376832 | gnu_tar.tar xv -C $output_path
-ls -lah /tmp
+ls -lah $output_path
 
 set -x
