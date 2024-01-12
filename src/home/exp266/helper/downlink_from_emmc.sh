622777eefe3f76ec1d61c292a3324d7cc1211e54
yasiu <hi@y4s.io>
2024-01-12 16:57:18 +0000

    different unpack settings

--- src/home/exp266/helper/downlink_from_emmc.sh
+++ src/home/exp266/helper/downlink_from_emmc.sh
@@ -12,7 +12,7 @@ mkdir -p $output_path
 
 #tar cf - A | gzip -9 > B.tar.gz
 #dd if=/dev/mmcblk0 bs=512 skip=13680640 count=376832 of=$output_path/$name.tar
-dd if=/dev/mmcblk0 bs=512 skip=13680640 count=376832 | gzip -9 > $output_path/$name.tar.gz
+dd if=/dev/mmcblk0 bs=512 skip=13680640 count=376832 | gzip -1 -v > $output_path/$name.tar.gz
 gnu_tar.tar tv $output_path/$name.tar.gz
 ls -lah $output_path
 
