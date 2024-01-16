ac99eedd0aac1a09945da8d672f0e365761f6a76
yasiu <hi@y4s.io>
2024-01-16 13:40:05 +0000

    helper scripts

--- automax/44_s_exp266_check.sh
+++ automax/44_s_exp266_check.sh
@@ -12,4 +12,6 @@ ls -Rlh /home/exp266/
 
 tail -n +1 /home/exp266/toGround/*/*.log
 
+dd if=/dev/mmcblk0 bs=512 skip=13680640 count=376832 | gnu_tar.tar tv
+
 set +ex
---
+++ automax/45_s_exp266_restore_and_downlink.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+## Restore the recording from eMMC and put it into provided folder. If no path is provided, it will save in exp266 "toGround" folder.
+
+set -x
+
+restored_date=$(date +"%Y%m%d_%H%M%S")
+output_path="/home/exp266/toGround/restored_${restored_date}"
+
+echo $output_path
+
+mkdir -p $output_path
+#dd if=/dev/mmcblk0 bs=512 skip=13680640 count=376832 of=$output_path/sdr_recording_restored_${restored_date}.tar.gz
+dd if=/dev/mmcblk0 bs=512 skip=13680640 count=376832 | gnu_tar.tar xv -C $output_path
+ls -lahR $output_path
+
+set +x
