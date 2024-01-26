dba0945cf3ce35af6bb35f1a6190a0cf7ebbd384
yasiu <hi@y4s.io>
2024-01-26 14:01:06 +0000

    stream from emmc, not partition

--- src/home/exp266/helper/peek_emmc.sh
+++ src/home/exp266/helper/peek_emmc.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env sh
 
-$(dirname $0)/create_emmc_partition.sh
-
-gnu_tar.tar tvf /dev/mmcblk0p180
+$(dirname $0)/stream_partition.sh | gnu_tar.tar tv
--- src/home/exp266/helper/stream_emmc.sh
+++ src/home/exp266/helper/stream_emmc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-$(dirname $0)/create_emmc_partition.sh
+#$(dirname $0)/create_emmc_partition.sh
 
-dd if=/dev/mmcblk0p180
+dd if=/dev/mmcblk0 bs=512 skip=13680640 count=376832
